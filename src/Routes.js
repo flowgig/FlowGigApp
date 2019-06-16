ae01a5babd4544de1308eca3dc2c8842b3c035c0
Benjamin Dehli <benjamin@arkitektum.no>
2019-06-16 22:45:58 +0200

    Add routes for gigs

--- src/Routes.js
+++ src/Routes.js
@@ -5,12 +5,14 @@ import App from './containers/App';
 import HomePage from './containers/HomePage';
 import SignUpPage from './containers/SignUpPage';
 import DraggablePage from './containers/DraggablePage';
+import GigsPage from './containers/GigsPage';
 
 export default () => (
   <App>
     <Switch>
       <Route path={routes.DRAGGABLE} component={DraggablePage} />
       <Route path={routes.SIGNUP} component={SignUpPage} />
+      <Route path={routes.GIGS} component={GigsPage} />
       <Route exact path={routes.HOME} component={HomePage} />
     </Switch>
   </App>
