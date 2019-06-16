729218993dbb449c8bed1c79af597c987bddfb29
Benjamin Dehli <benjamin@arkitektum.no>
2019-06-16 17:10:05 +0200

    Implement Redux and routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Root from './containers/Root';
+import theme from './theme';
+import { configureStore, history } from './store/configureStore';
+import './App.module.scss';
+
+const store = configureStore();
+
+render(
+  <ThemeProvider theme={theme}>
+    <AppContainer>
+      <Root store={store} history={history} />
+    </AppContainer>
+  </ThemeProvider>,
+  document.getElementById('root')
+);
+
+if (module.hot) {
+  module.hot.accept('./containers/Root', () => {
+    const NextRoot = require('./containers/Root').default;
+    render(
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppContainer>
+          <NextRoot store={store} history={history} />
+        </AppContainer>
+      </ThemeProvider>,
+      document.getElementById('root')
+    );
+  });
+}
