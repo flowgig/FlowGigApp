6cb76dd652ed99100d5e33436932327dff4e0daf
Benjamin Dehli <benjamin@arkitektum.no>
2019-06-23 22:26:55 +0200

    Use dashboard component for home page

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import SignIn from '../components/SignIn';
+import Dashboard from '../components/Dashboard';
 
 export default class HomePage extends Component {
   render() {
-    return <SignIn />;
-}
+    return <Dashboard />;
+  }
 }
