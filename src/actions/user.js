bda5c8a40fdd4dec464583c7f41e5cbeb7e17a34
Benjamin Dehli <benjamin@arkitektum.no>
2019-06-30 14:56:21 +0200

    Add actions for user

--- src/actions/user.js
+++ src/actions/user.js
@@ -6,3 +6,15 @@ export const updateUser = (user) => dispatch => {
         payload: user,
     })
 };
+
+export const logOut = (user) => dispatch => {
+    dispatch({
+        type: UPDATE_USER,
+        payload: null,
+    })
+};
+
+export const isLoggedIn = () => (dispatch, getState) => {
+  const user = getState() && getState().user ? getState().user : null;
+    return user && user.name && user.email;
+};
