8beaae3c9850207cae808349237e2fb7b0319419
Benjamin Dehli <superelg@gmail.com>
2020-02-27 22:16:54 +0100

    Replace electron with PWA service worker

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+import React, { Component } from 'react';
+import * as serviceWorker from 'serviceWorker';
+import { hydrate, render } from "react-dom";
 
+import { AppContainer } from 'react-hot-loader';
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import Root from './containers/Root';
-import theme from './theme';
-import { configureStore, history } from './store/configureStore';
+import Root from 'containers/Root';
+import theme from 'theme';
+import configureStore, {history} from 'utils/configureStore';
 
 const store = configureStore();
 
-
-render(
-  <ThemeProvider theme={theme}>
-    <AppContainer>
-      <Root store={store} history={history} />
-    </AppContainer>
-  </ThemeProvider>,
-  document.getElementById('root')
-);
-
-if (module.hot) {
-  module.hot.accept('./containers/Root', () => {
-    const NextRoot = require('./containers/Root').default;
-    render(
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <AppContainer>
-          <NextRoot store={store} history={history} />
-        </AppContainer>
-      </ThemeProvider>,
-      document.getElementById('root')
-    );
-  });
+class Main extends Component {
+    render() {
+        return (
+          <ThemeProvider theme={theme}>
+            <AppContainer>
+              <Root store={store} history={history} />
+            </AppContainer>
+          </ThemeProvider>
+      )
+    }
 }
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<Main />, rootElement);
+} else {
+  render(<Main />, rootElement);
+}
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
