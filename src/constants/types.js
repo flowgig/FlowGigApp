6baae5b6f64307c08fedf11dae667510fc322c2d
Benjamin Dehli <superelg@gmail.com>
2024-03-26 20:19:00 +0100

    Replace content with breeze-next boilerplate

--- src/constants/types.js
+++
@@ -1,2 +0,0 @@
-export const UPDATE_USER = 'UPDATE_USER';
-export const FETCH_GIGS = 'FETCH_GIGS';
--- src/helpers/apiUrlHelpers.js
+++
@@ -1,4 +0,0 @@
-export const getFlowGigApiUrl = () => {
-  const environmentSlug = process.env.REACT_APP_ENVIRONMENT === 'dev' || process.env.REACT_APP_ENVIRONMENT === 'test' ? process.env.REACT_APP_ENVIRONMENT : 'www';
-  return `https://${environmentSlug}.flowgig.com/api`;
-};
