94d6cd740e02fe43cd0b9d95a85beba65e2f30a1
Benjamin Dehli <superelg@gmail.com>
2024-03-31 18:35:06 +0200

    Add FlowGig colors to theme

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,7 @@
 "use client";
 import { Roboto } from "next/font/google";
 import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
 
 const roboto = Roboto({
     weight: ["300", "400", "500", "700"],
@@ -10,19 +11,27 @@ const roboto = Roboto({
 
 const theme = createTheme({
     palette: {
-        mode: "light"
+        mode: "light",
+        primary: {
+            main: "#C32A22"
+        },
+        secondary: {
+            main: "#19857b"
+        },
+        error: {
+            main: red.A400
+        },
+        background: {
+            default: "#fff"
+        }
     },
     typography: {
         fontFamily: roboto.style.fontFamily
     },
     components: {
-        MuiAlert: {
-            styleOverrides: {
-                root: ({ ownerState }) => ({
-                    ...(ownerState.severity === "info" && {
-                        backgroundColor: "#60a5fa"
-                    })
-                })
+        MuiAppBar: {
+            colorPrimary: {
+                backgroundColor: "#272b2f"
             }
         }
     }
